5bb7bf623bdf245cae8077ca0ef5f451dffcab8f
Diego <diegolima.almeida93@gmail.com>
2023-10-30 15:46:08 -0300

    feat: page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,16 @@
 import * as C from "@/components";
 import ScrollSpy from "react-ui-scrollspy";
 import { inter } from "@/fonts";
+import Head from "next/head";
 
 const Home = () => {
   return (
     <div
       className={`${inter.className} bg-light-blue2 text-light-blue12 dark:bg-dark-blue2 dark:text-dark-blue12`}
     >
+      <Head>
+        <title>Diego Lima</title>
+      </Head>
       <div className="lg:min-h-screen lg:max-w-7xl lg:m-auto lg:px-16">
         <div className="lg:flex lg:justify-between lg:gap-4">
           <div className="lg:max-h-screen lg:w-2/5 lg:sticky lg:top-0 lg:py-24">
