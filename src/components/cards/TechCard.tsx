be68b224fcaba678dec0964a2d370e4eaf83f326
Diego <diegolima.almeida93@gmail.com>
2023-10-30 15:42:36 -0300

    update: tech card css, remove aria label

--- src/components/cards/TechCard.tsx
+++ src/components/cards/TechCard.tsx
@@ -12,14 +12,13 @@ export const TechCard = ({ techName }: TechCardProps) => {
       className="w-full m-auto flex flex-col gap-4 p-8 rounded-md"
       style={{ backgroundColor: color }}
     >
-      <div
-        className="w-full flex justify-center items-center text-white text-9xl"
-        aria-label={`Logo da tecnologia ${name}`}
-      >
+      <div className="w-full flex justify-center items-center text-white text-9xl drop-shadow-lg">
         {logo}
       </div>
-      <div className="h-20 flex justify-center items-center">
-        <h2 className="text-white text-2xl font-bold text-center">{name}</h2>
+      <div className="h-20 flex justify-center items-center text-sha">
+        <h2 className="text-white text-2xl font-bold text-center drop-shadow-lg">
+          {name}
+        </h2>
       </div>
     </div>
   );
